f12c1a6824a139d6b1b6575a90ac0f912cc8813e
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-12-28 10:42:24 +0100

    add item controller + methods

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,7 @@
+class ItemsController < ApplicationController
+  def show
+	@course = Course.friendly.find(params[:course_id])
+    @chapter = @course.chapters.friendly.find(params[:chapter_id])
+	@item = @chapter.items.friendly.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/show'
+
   get 'show/index'
 
   devise_for :users
