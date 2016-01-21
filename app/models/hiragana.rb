8bb716dc59097b20f0b06e40e1f1aebd619c23e3
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-21 23:51:25 +0100

    migration doesn't work

--- app/models/hiragana.rb
+++ app/models/hiragana.rb
@@ -3,5 +3,5 @@ class Hiragana < ActiveRecord::Base
   validates :ideoone, presence: true
   validates :ideotwo, presence: true
   validates :transcription, presence: true
-  validates :upletter, length: { maximum: 2 }, presence: true
+  validates :upletter, presence: true
 end
