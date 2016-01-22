c703936615da7acc12b25dc1a9e7ef9b3b041f81
Baptiste Becmeur <baptistebecmeur@gmail.com>
2016-01-22 22:19:37 +0100

    cloudinary

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,12 +2,4 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-
-  def public_id
-    if model.try(:title)
-      return model.title.parameterize
-    else
-      return model.object_id
-    end
-  end
 end
